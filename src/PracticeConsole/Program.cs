4ee1fa117fa9a83fe575c6ce53cb78a794561096
Don Welch <dwelch@nait.ca>
2021-09-13 20:40:58 -0600

    create new practice console project

---
+++ src/PracticeConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//using static System.Console;
+
+namespace PracticeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //WriteLine("Hello world");
+        }
+    }
+}
