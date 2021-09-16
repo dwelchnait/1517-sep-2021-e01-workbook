ec7dfccddbf50d40f2a0ff46209c5ce28a5bea6d
Don Welch <dwelch@nait.ca>
2021-09-15 20:14:04 -0600

    setup classes for object demo, create enum definition

---
+++ src/PracticeConsole/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole.Data
+{
+    class Person
+    {
+        //each instance of this class will represent an individual
+        //This class will define the following characteristics of a person
+        //  First Name, Last Name, list of employment positions
+    }
+}
---
+++ src/PracticeConsole/SupervisoryLevel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole.Data
+{
+    //SupervisoryLevel is the data type name of the enum  
+    public enum SupervisoryLevel
+    {
+        Entry,          // = 0
+        TeamMember,     // = 1
+        TeamLeader,     // = 2
+        Supervisor,     // = 3
+        DepartmentHead, // = 4
+        Owner           // = 5
+    }
+}
