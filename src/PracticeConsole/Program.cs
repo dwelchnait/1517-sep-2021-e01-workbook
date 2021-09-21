48905d8b493071e4749f1ccc8ad03511efa80f18
Don Welch <dwelch@nait.ca>
2021-09-20 20:46:59 -0600

    complete basic class demo and driver

--- src/PracticeConsole/Program.cs
+++ src/PracticeConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PracticeConsole.Data;
+using System;
+using System.Collections.Generic;
 //using static System.Console;
 
 namespace PracticeConsole
@@ -7,8 +9,71 @@ namespace PracticeConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //WriteLine("Hello world");
+            
+            List<Employment> jobs = new List<Employment>();
+
+            //declare and load Employment instance separately
+            //default constructor
+            Employment job1 = new Employment();
+            //property set
+            job1.Title = "Lab assistance";
+            //the Level property has a private set so you cannot directly
+            //  assign a value to te Level property
+            //Instead, use the method provided which will assign the
+            //  given argument value to the Level property internally
+            job1.SetEmployeeResponsibility(SupervisoryLevel.TeamLeader);
+            job1.Years = 7.4;
+
+            jobs.Add(job1); //add to the jobs List<T> where T is Employment
+
+            //declare and load Employment instance using constructor
+            //we can reuse the Employment variable job1 BECAUSE we are creating
+            //  a new instance of the Employment class
+            //greedy constructor
+            job1 = new Employment("Lab Research", SupervisoryLevel.TeamMember, 5.8);
+            jobs.Add(job1); //add to the jobs List<T> where T is Employment
+
+
+            //declare and load Employment instance using object instantiation
+            job1 = new Employment()
+            {
+                Title = "Gander Cooking Club",
+                Years = 1.2
+            };
+            job1.SetEmployeeResponsibility(SupervisoryLevel.Owner);
+            jobs.Add(job1); //add to the jobs List<T> where T is Employment
+
+            Person me = new Person()
+            {
+                FirstName = "don",
+                LastName = "welch",
+                EmploymentPositions = jobs
+            };
+
+            //display the contents of Person
+            Console.WriteLine("Person:\n");
+            Console.WriteLine($"{me.LastName}, {me.FirstName}: \n");
+            Console.WriteLine("Past/Present Employment:\n");
+            foreach (Employment item in me.EmploymentPositions)
+            {
+                Console.WriteLine($"\t{item.ToString()}");
+            }
+            
+            Employment badjob;
+            Person badperson;
+            try
+            {
+                //badjob = new Employment("testing bads", SupervisoryLevel.TeamMember, 5.8);
+                badperson = new Person()
+                {
+                    FirstName = "don",
+                    LastName = "welch"
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"**********\n{ex.Message}\n**************");
+            }
         }
     }
 }
---
+++ src/PracticeConsole/Utilities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole
+{
+    public static class Utilities
+    {
+        //these types of class should not hold and retain data
+        // "static" makes only one active copy of the class 
+        //      when the program runs
+        //when using a static class, the developer does not create
+        //   an individual instance, instead the  developer will
+        //   reference items in the static class by ClassName.Method()
+        public static bool IsEmpty(string value)
+        {
+            bool valid = false;
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                valid = true;
+            }
+            return valid;
+        }
+
+        //overload
+        public static bool IsPositive(double value)
+        {
+            bool valid = false;
+            if (value >= 0.0)
+            {
+                valid = true;
+            }
+            return valid;
+        }
+        public static bool IsPositive(int value)
+        {
+            bool valid = false;
+            if (value >= 0)
+            {
+                valid = true;
+            }
+            return valid;
+        }
+    }
+}
