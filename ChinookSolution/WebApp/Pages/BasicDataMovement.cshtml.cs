bb534f2e9cd8d66b300f38767cab4e158cb059e2
Don Welch <dwelch@nait.ca>
2021-10-18 20:49:24 -0600

    complete jquery lib setup and investigate my first web page using OnGet

--- ChinookSolution/WebApp/Pages/BasicDataMovement.cshtml.cs
+++ ChinookSolution/WebApp/Pages/BasicDataMovement.cshtml.cs
@@ -9,8 +9,30 @@ namespace WebApp.Pages
 {
     public class BasicDataMovementModel : PageModel
     {
+        //data members
+        public string MyName;
+
+        //properties
+
+        //contructors
+
+
+        //behaviours (aka methods)
         public void OnGet()
         {
+            //Default Request is OnGet(),
+            //executed each time the page is enterd and/or refreshed
+            //  if no form post with a RedirecToPage()
+            Random rdn = new Random();
+            int oddeven = rdn.Next(0, 25);
+            if (oddeven % 2 == 0)
+            {
+                MyName = $"Don is {oddeven}";
+            }
+            else
+            {
+                MyName = null;
+            }
         }
     }
 }
