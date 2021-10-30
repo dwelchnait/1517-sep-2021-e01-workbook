5d119ac97824f812280fb42b393adf821b794d8b
Don Welch <dwelch@nait.ca>
2021-10-29 21:28:59 -0600

    Implementaiton of application library backend: Part I

---
+++ WestWindSolution/WestWindSystem/BLL/WestWindServices.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+#region Additinal 
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+#endregion
+
+namespace WestWindSystem.BLL
+{
+    public class WestWindServices
+    {
+        //I need an instance of he WestWindContext class
+        private readonly WestWindContext _context;
+
+        //I need a constructor of this class to initialize my instance of WestWindContext
+        internal WestWindServices(WestWindContext context)
+        {
+            _context = context;
+        }
+
+        //create a method (service) that will retrieve the BuildVersion record
+        //this will be called from the web app, thus it needs to be public
+        //this comes part of the class library's (application library) public interface
+        public BuildVersion GetBuildVersion()
+        { 
+            //going to your context instance, calling on the DbSet  property,
+            // which will retrieve your data from the database, then return
+            // the First record from the database collection (set, dataset) OR
+            // if not data was returned from sql, set the receiving variable to null.
+            BuildVersion info = _context.BuildVersions.FirstOrDefault();
+            return info;
+        }
+    }
+}
---
+++ WestWindSolution/WestWindSystem/DAL/WestWindContext.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.EntityFrameworkCore;
+using WestWindSystem.Entities;
+#endregion
+
+namespace WestWindSystem.DAL
+{
+    internal class WestWindContext:DbContext
+    {
+        public WestWindContext()
+        {
+
+        }
+
+        //options? things like DatabaseProvider(MSQL | Oracle)
+        //                     what is the connection string
+        //                      what type of Authorization
+        //                      that is the name of the database
+        //this Dependency injection is to help us "externalize" our reliance on objects
+        //      that are "out-of-scope" of your code
+        //example wheels on a car are attached by bolts so they can be change if needed
+        //         (software updates).
+        //Consider if the wheels wer welded to your axils. How difficult would it be
+        //      to change the tires {update software).
+        //the options can have the necessary values to context our system to the
+        //      software in the EntityFramework which is responsible for the actually
+        //      transfer of data from sql to the program and back.
+        public WestWindContext(DbContextOptions<WestWindContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<BuildVersion> BuildVersions { get; set; }
+    }
+}
