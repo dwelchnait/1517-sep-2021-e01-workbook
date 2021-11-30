04341ff8e7d24a36da9d8115552fdeea272c71e8
Don Welch <dwelch@nait.ca>
2021-11-29 20:52:16 -0700

    code code-behind OnPost events

--- WestWindSolution/WebApp/Pages/CRUDPage.cshtml.cs
+++ WestWindSolution/WebApp/Pages/CRUDPage.cshtml.cs
@@ -32,13 +32,21 @@ namespace WebApp.Pages
         [TempData]
         public string FeedbackMessage { get; set; }
 
-        public bool HasFeedback => !string.IsNullOrWhiteSpace(FeedbackMessage); 
+        public bool HasFeedback => !string.IsNullOrWhiteSpace(FeedbackMessage);
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public bool HasError => !string.IsNullOrWhiteSpace(ErrorMessage);
         #endregion
 
         [BindProperty(SupportsGet =true)]
         public string territoryid { get; set; }
 
         // this holds the territory record we are maintaining
+        // this is a two way passage of data
+        // on the OnPost event (Request) the system will fill your Bound Property
+        //    with the current contents of the data fields
         [BindProperty]
         public Territory territoryInfo { get; set; }
 
@@ -56,25 +64,78 @@ namespace WebApp.Pages
 
         public IActionResult OnPostAdd()
         {
-            //TODO: a comment in Add
-            return Page();
+            //this has to be done in a user freindly error handling environment
+            try
+            {
+                // call the appropriate TerritoryService to attempt to add the
+                //    data to the database
+                FeedbackMessage = "Teritory has been added";
+                return RedirectToPage(new { territoryid = territoryid });
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = GetInnerException(ex).Message;
+                regionInfo = _regionservices.Region_List(); //retreive data for the dropdownlist
+                return Page(); //Page IS NOT the same as RedirectToPage, does not go to OnGet
+            }
+
         }
         public IActionResult OnPostUpdate()
         {
-            //TODO: a comment in update
-            return Page();
+            //this has to be done in a user freindly error handling environment
+            try
+            {
+                // call the appropriate TerritoryService to attempt to update the
+                //    data on the database
+                FeedbackMessage = "Territory has been updated";
+                return RedirectToPage(new { territoryid = territoryid });
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = GetInnerException(ex).Message;
+                regionInfo = _regionservices.Region_List(); //retreive data for the dropdownlist
+                return Page(); //Page IS NOT the same as RedirectToPage, does not go to OnGet
+            }
+
         }
 
         public IActionResult OnPostRemove()
         {
-            //TODO: a comment in remove
-            return Page();
+            
+            //this has to be done in a user freindly error handling environment
+            try
+            {
+                // call the appropriate TerritoryService to attempt to remove the
+                //    data from the database
+                FeedbackMessage = "Territory has been removed";
+                return RedirectToPage(new { territoryid = "" });
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = GetInnerException(ex).Message;
+                regionInfo = _regionservices.Region_List(); //retreive data for the dropdownlist
+                return Page(); //Page IS NOT the same as RedirectToPage, does not go to OnGet
+            }
+            
         }
 
         public IActionResult OnPostClear()
         {
-            //TODO: a comment in clear
-            return Page();
+            
+
+            return RedirectToPage(new { territoryid = ""});
+        }
+        public IActionResult OnPostBack()
+        {
+            return RedirectToPage("/PartialFilterSearch");
+        }
+
+        private Exception GetInnerException(Exception ex)
+        {
+            //drill down to the REAL ERROR MESSAGE
+            while (ex.InnerException != null)
+                ex = ex.InnerException;
+            return ex;
         }
     }
 }
